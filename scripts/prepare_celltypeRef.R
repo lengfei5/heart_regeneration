3aa07ab3674e6bed29f010a28d0baf645be6d5e5
jingkui.wang <jingkui.wang@imp.ac.at>
2021-11-18 12:15:47 +0100

    stop here for the cardiomyocyte and start the non-cardiomyocyte

--- scripts/prepare_celltypeRef.R
+++ scripts/prepare_celltypeRef.R
@@ -62,6 +62,8 @@ save(metadata, counts, file = paste0(RdataDir, 'metadata_counts_week0.week2.Rdat
 ##########################################
 # make Seurat object with metadata and counts  
 ##########################################
+load(file = paste0(RdataDir, 'metadata_counts_week0.week2.Rdata'))
+
 myo <- CreateSeuratObject(counts = counts, project = "adult", min.cells = 50, min.features = 500)
 myo = AddMetaData(myo, metadata, col.name = NULL) 
 
@@ -82,29 +84,37 @@ aa <- RunPCA(aa, verbose = FALSE, weight.by.var = TRUE)
 ElbowPlot(aa, ndims = 30)
 
 aa <- FindNeighbors(aa, dims = 1:10)
-aa <- FindClusters(aa, verbose = FALSE, algorithm = 3, resolution = 0.7)
+aa <- FindClusters(aa, verbose = FALSE, algorithm = 3, resolution = 0.5)
 
-####### KEEP this umap configuration: nfeatures = 2000, dims = 1:20, n.neighbors = 30 and min.dist = 0.05 for week0 dataset
-#######
-aa <- RunUMAP(aa, dims = 1:20, n.neighbors = 50, min.dist = 0.1) 
+saveRDS(aa, file = paste0(RdataDir, 'Seurat.obj_adultMiceHeart_week0.week2_Ren2020.rds'))
+
+####### KEEP the umap configuration for only week0: nfeatures = 2000, dims = 1:20, n.neighbors = 30 and min.dist = 0.05 
+####### umap configuraiton for week0 and week2: nfeatures = 3000, dims = 1:20, n.neighbors = 30/50, min.dist = 0.1
+aa <- RunUMAP(aa, dims = 1:20, n.neighbors = 30, min.dist = 0.1) 
 
 p1 = DimPlot(aa, reduction = 'umap', group.by = 'seurat_clusters')
 p2 = DimPlot(aa, reduction = "umap", group.by = c("CellType"))
 p1 + p2 
 
+p3 = DimPlot(aa, reduction = 'umap', group.by = 'condition')
+p4 = DimPlot(aa, reduction = 'umap', group.by = 'sample')
+(p1 + p2)/(p3 + p4) + ggsave(paste0(resDir, '/Umap_Ren2020_week0.week2_newClusters_vs_cellType.original_conditions_samples.pdf'), 
+                             width = 12, height = 8)
 
-p1 = DimPlot(aa, reduction = 'umap', group.by = 'condition')
-p2 = DimPlot(aa, reduction = 'umap', group.by = 'sample', pt.size = 1.0)
-p1 + p2
-
-
+# double check the cardiomyocyte markers from Elad 
 gg.examples =  c('Nppa', 'Nppb', 'Myh6', 'Tnnc1', 'Tnni3', 
                  'Tnnt2', 'Actn2', 'Gata4', 'Nkx2-5', 'Gja1', 
                  'Myl2', 'Tpm1', 'Ryr2', 'Atp2a2', 'Acta1')
 
-p3 = FeaturePlot(aa, reduction = 'umap', features = gg.examples)
+p5 = FeaturePlot(aa, reduction = 'umap', features = gg.examples)
 
+p2 / p5 + ggsave(paste0(resDir, '/Umap_cellType.original_FeatuerPlot_cardiomyocyoteMarkers.pdf'), 
+                  width = 14, height = 20)
 
+p6 = VlnPlot(aa, features = gg.examples, group.by = 'CellType')
+
+p6 + ggsave(paste0(resDir, '/Umap_VlnPlot_cardiomyocyoteMarkers.pdf'), 
+            width = 14, height = 10)
 
 p1 + p2 + ggsave(paste0(resDir, '/Umap_newClusters_vs_cellType.original_v2.pdf'), 
                  width = 12, height = 8)
@@ -114,17 +124,38 @@ p1 + p2 + ggsave(paste0(resDir, '/Umap_newClusters_vs_cellType.original_v2.pdf')
 FeaturePlot(aa, reduction = 'umap', features = c('Csf1r', 'Cd163'))
 FeaturePlot(aa, reduction = 'umap', features = c('S100a3', 'S100a9'))
 
-##########################################
-# heatmap of marker genes
-##########################################
-# remove the mitochrio marker genes
-jj = grep('^mt-', rownames(aa)) 
-aa = aa[-jj, ]
 
-aa.markers <- FindAllMarkers(aa, only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.25)
-aa.markers %>%
-  group_by(cluster) %>%
-  top_n(n = 10, wt = avg_log2FC) -> top10
-DoHeatmap(aa, features = top10$gene) + NoLegend() + ggsave(paste0(resDir, '/heatmap_markerGenes_rmMt.pdf'), 
-                                                           width = 12, height = 16)
+
+##########################################
+# define the subclusters of CM (to do)
+##########################################
+
+
+
+##########################################
+# heatmap of marker genes  
+##########################################
+Refine.clustering.annotaiton = FALSE
+if(Refine.clustering.annotaiton){
+  # remove the mitochrio marker genes
+  jj = grep('^mt-', rownames(aa)) 
+  aa = aa[-jj, ]
+  
+  aa.markers <- FindAllMarkers(aa, only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.25)
+  aa.markers %>%
+    group_by(cluster) %>%
+    top_n(n = 10, wt = avg_log2FC) -> top10
+  DoHeatmap(aa, features = top10$gene) + NoLegend() + ggsave(paste0(resDir, '/heatmap_markerGenes_rmMt.pdf'), 
+                                                             width = 12, height = 16)
+}
+
+
+########################################################
+########################################################
+# Section : 
+# 
+########################################################
+########################################################
+
+
 
