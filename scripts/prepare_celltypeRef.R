e2fc2bfebbeb53ac78d13873b80e75e867427861
jingkui.wang <jingkui.wang@imp.ac.at>
2021-11-22 10:37:36 +0100

    save the seurat integration using rpca for two refs

--- scripts/prepare_celltypeRef.R
+++ scripts/prepare_celltypeRef.R
@@ -25,6 +25,9 @@ require(sctransform)
 require(ggplot2)
 library(dplyr)
 library(patchwork)
+require(tictoc)
+
+Normalization = 'lognormal' # ('lognormal or SCT')
 
 ########################################################
 ########################################################
@@ -77,8 +80,24 @@ myo = subset(myo, subset = nFeature_RNA > 500 & nFeature_RNA < 5000 & nCount_RNA
 
 aa = myo
 
-aa <- SCTransform(aa, assay = "RNA", verbose = FALSE, variable.features.n = 3000, return.only.var.genes = FALSE, 
-                  min_cells=5) 
+if(SCT.normalization){
+  aa <- SCTransform(aa, assay = "RNA", verbose = FALSE, variable.features.n = 3000, return.only.var.genes = FALSE, 
+                    min_cells=5) 
+  
+}else{
+  aa <- NormalizeData(aa, normalization.method = "LogNormalize", scale.factor = 10000)
+  aa <- FindVariableFeatures(aa, selection.method = "vst", nfeatures = 3000)
+  
+  plot1 <- VariableFeaturePlot(aa)
+  
+  top10 <- head(VariableFeatures(aa), 10)
+  plot2 <- LabelPoints(plot = plot1, points = top10, repel = TRUE)
+  plot1 + plot2
+  
+  all.genes <- rownames(aa)
+  aa <- ScaleData(aa, features = all.genes)
+  
+}
 
 aa <- RunPCA(aa, verbose = FALSE, weight.by.var = TRUE)
 ElbowPlot(aa, ndims = 30)
@@ -86,11 +105,16 @@ ElbowPlot(aa, ndims = 30)
 aa <- FindNeighbors(aa, dims = 1:10)
 aa <- FindClusters(aa, verbose = FALSE, algorithm = 3, resolution = 0.5)
 
-saveRDS(aa, file = paste0(RdataDir, 'Seurat.obj_adultMiceHeart_week0.week2_Ren2020.rds'))
+saveRDS(aa, file = paste0(RdataDir, 'Seurat.obj_adultMiceHeart_week0.week2_Ren2020_seuratNormalization.rds'))
 
-####### KEEP the umap configuration for only week0: nfeatures = 2000, dims = 1:20, n.neighbors = 30 and min.dist = 0.05 
-####### umap configuraiton for week0 and week2: nfeatures = 3000, dims = 1:20, n.neighbors = 30/50, min.dist = 0.1
-aa <- RunUMAP(aa, dims = 1:20, n.neighbors = 30, min.dist = 0.1) 
+### KEEP the umap configuration for only week0: nfeatures = 2000, dims = 1:20, n.neighbors = 30 and min.dist = 0.05 (sctransform)
+### umap configuraiton for week0 and week2: nfeatures = 3000, dims = 1:20, n.neighbors = 30/50, min.dist = 0.1 (sctransform)
+### umap configuration for week0 and week2: nfeatures = 3000, dims = 1:20, n.neighbors = 20, min.dist = 0.05 (seurat.norm)
+aa = RunUMAP(aa, dims = 1:20, n.neighbors = 30, min.dist = 0.1)
+saveRDS(aa, file =  paste0(RdataDir, 'Seurat.obj_adultMiceHeart_week0.week2_Ren2020_SCT_umap.rds'))
+
+#aa <- RunUMAP(aa, dims = 1:20, n.neighbors = 20, min.dist = 0.05)
+#saveRDS(aa, file =  paste0(RdataDir, 'Seurat.obj_adultMiceHeart_week0.week2_Ren2020_seuratNormalization_umap.rds'))
 
 p1 = DimPlot(aa, reduction = 'umap', group.by = 'seurat_clusters')
 p2 = DimPlot(aa, reduction = "umap", group.by = c("CellType"))
@@ -98,7 +122,8 @@ p1 + p2
 
 p3 = DimPlot(aa, reduction = 'umap', group.by = 'condition')
 p4 = DimPlot(aa, reduction = 'umap', group.by = 'sample')
-(p1 + p2)/(p3 + p4) + ggsave(paste0(resDir, '/Umap_Ren2020_week0.week2_newClusters_vs_cellType.original_conditions_samples.pdf'), 
+(p1 + p2)/(p3 + p4) + 
+  ggsave(paste0(resDir, '/Umap_Ren2020_week0.week2_newClusters_vs_cellType.original_conditions_samples_seuratNorm.pdf'), 
                              width = 12, height = 8)
 
 # double check the cardiomyocyte markers from Elad 
@@ -108,15 +133,15 @@ gg.examples =  c('Nppa', 'Nppb', 'Myh6', 'Tnnc1', 'Tnni3',
 
 p5 = FeaturePlot(aa, reduction = 'umap', features = gg.examples)
 
-p2 / p5 + ggsave(paste0(resDir, '/Umap_cellType.original_FeatuerPlot_cardiomyocyoteMarkers.pdf'), 
+p2 / p5 + ggsave(paste0(resDir, '/Umap_cellType.original_FeatuerPlot_cardiomyocyoteMarkers_seuratNorm.pdf'), 
                   width = 14, height = 20)
 
 p6 = VlnPlot(aa, features = gg.examples, group.by = 'CellType')
 
-p6 + ggsave(paste0(resDir, '/Umap_VlnPlot_cardiomyocyoteMarkers.pdf'), 
+p6 + ggsave(paste0(resDir, '/Umap_VlnPlot_cardiomyocyoteMarkers_seuratNorm.pdf'), 
             width = 14, height = 10)
 
-p1 + p2 + ggsave(paste0(resDir, '/Umap_newClusters_vs_cellType.original_v2.pdf'), 
+p1 + p2 + ggsave(paste0(resDir, '/Umap_newClusters_vs_cellType.original_seuratNorm.pdf'), 
                  width = 12, height = 8)
 
 #DimPlot(aa, reduction = "umap", group.by = c("SubCluster"))
@@ -130,13 +155,12 @@ FeaturePlot(aa, reduction = 'umap', features = c('S100a3', 'S100a9'))
 ##########################################
 
 
-
 ##########################################
 # heatmap of marker genes  
 ##########################################
 Refine.clustering.annotaiton = FALSE
 if(Refine.clustering.annotaiton){
-  # remove the mitochrio marker genes
+  # remove the mitochonio marker genes
   jj = grep('^mt-', rownames(aa)) 
   aa = aa[-jj, ]
   
@@ -148,12 +172,13 @@ if(Refine.clustering.annotaiton){
                                                              width = 12, height = 16)
 }
 
-
 ########################################################
 ########################################################
 # Section : adult non-caridomyocyte single cell dataset 
 # the dataset is from Forte et al. 2020 
 # processed dataset from the collaborate Shoval
+# in which the lognormal normalization was used and no batch correction or data integration were performed
+# because they think it is not required for this dataset, or the batch difference is minor
 ########################################################
 ########################################################
 dataDir = '/groups/tanaka/People/current/jiwang/projects/heart_regeneration/data/'
@@ -178,5 +203,151 @@ p2 = DimPlot(aa, reduction = "umap_0.05", group.by = c("timepoints"))
 p1 + p2 + ggsave(paste0(resDir, '/Forte2020_Umap_clusters._cellType.original.pdf'), 
                  width = 16, height = 8)
 
+saveRDS(aa, file = paste0(RdataDir, 'Forte2020_processedSeurat.obj.from.Shoval.rds'))
 
+##########################################
+# explore the normalization and intergration 
+##########################################
+if(Normalization == 'SCT'){
+  
+  tic()
+  aa <- SCTransform(aa, assay = "RNA", verbose = FALSE, variable.features.n = 3000, 
+                    return.only.var.genes = FALSE, ncells = 5000, conserve.memory = FALSE,
+                    min_cells=5) 
+  toc()
+  
+  saveRDS(aa, file = paste0(RdataDir, 'Forte2020_SCTnorm_allgenes.rds'))
+  
+}else{
+  aa <- NormalizeData(aa, normalization.method = "LogNormalize", scale.factor = 10000)
+  aa <- FindVariableFeatures(aa, selection.method = "vst", nfeatures = 3000)
+  
+  plot1 <- VariableFeaturePlot(aa)
+  
+  top10 <- head(VariableFeatures(aa), 10)
+  plot2 <- LabelPoints(plot = plot1, points = top10, repel = TRUE)
+  plot1 + plot2
+  
+  all.genes <- rownames(aa)
+  aa <- ScaleData(aa, features = all.genes)
+  
+  saveRDS(aa, file = paste0(RdataDir, 'Forte2020_logNormalize_allgenes.rds'))
+  
+} 
 
+aa <- RunPCA(aa, verbose = FALSE, weight.by.var = TRUE)
+ElbowPlot(aa, ndims = 30)
+
+aa <- FindNeighbors(aa, dims = 1:30)
+aa <- FindClusters(aa, verbose = FALSE, algorithm = 3, resolution = 0.5)
+
+aa <- RunUMAP(aa, dims = 1:30, n.neighbors = 50, min.dist = 0.05) 
+p1 = DimPlot(aa, reduction = 'umap', group.by = 'my_annot') + ggtitle(paste0(Normalization, ' umap'))
+
+p0 = DimPlot(aa, reduction = 'umap_0.05', group.by = 'my_annot') + ggtitle('original umap')
+p0 + p1 +  ggsave(paste0(resDir, '/Forte2020_myumap_cellType.Shoval_', Normalization, '.pdf'), 
+               width = 16, height = 8)
+
+p2 = DimPlot(aa, reduction = "umap", group.by = c("timepoints"))
+
+p1 + p2 + ggsave(paste0(resDir, '/Forte2020_Umap_myclusters_cellType.Shoval_', Normalization, '.pdf'), 
+                 width = 16, height = 8)
+
+##########################################
+# integrate Ren2020 and Forte2020 to have one reference using SCTransform and RPCA from Seurat
+# original code from https://satijalab.org/seurat/articles/integration_rpca.html
+# 
+##########################################
+if(Normalization == 'SCT'){
+  cms = readRDS(file =  paste0(RdataDir, 'Seurat.obj_adultMiceHeart_week0.week2_Ren2020_SCT_umap.rds'))
+  
+  ref.list = list(aa, cms)
+  
+  features <- SelectIntegrationFeatures(object.list = ref.list, nfeatures = 3000, assay = c('SCT', 'SCT'))
+  #features <- SelectIntegrationFeatures(object.list = ifnb.list, nfeatures = 3000)
+  ref.list <- PrepSCTIntegration(object.list = ref.list, anchor.features = features)
+  
+  ref.list <- lapply(X = ref.list, FUN = RunPCA, features = features)
+  
+  # this command creates an 'integrated' data assay
+  ref.anchors <- FindIntegrationAnchors(object.list = ref.list, normalization.method = "SCT",
+                                        anchor.features = features, dims = 1:30, reduction = "rpca", k.anchor = 20)
+  
+  ref.sct <- IntegrateData(anchorset = ref.anchors, normalization.method = "SCT", dims = 1:30)
+  
+  ref.sct <- RunPCA(ref.sct, verbose = FALSE)
+  ref.sct <- RunUMAP(ref.sct, reduction = "pca", dims = 1:30)
+  
+  DefaultAssay(ref.sct) <- "integrated"
+  
+  # Visualization
+  p1 <- DimPlot(ref.sct, reduction = "umap", group.by = "stim")
+  p2 <- DimPlot(ref.sct, reduction = "umap", group.by = "seurat_annotations", label = TRUE,
+                repel = TRUE)
+  p1 + p2
+}else{
+  aa = readRDS(file = paste0(RdataDir, 'Forte2020_logNormalize_allgenes.rds'))
+  
+  cms = readRDS(file =  paste0(RdataDir, 'Seurat.obj_adultMiceHeart_week0.week2_Ren2020_seuratNormalization_umap.rds'))
+  aa$dataset = 'Forte2020'
+  cms$dataset = 'Ren2020'
+  aa$annot.ref = aa$my_annot
+  cms$annot.ref = cms$CellType
+  features.common = intersect(rownames(aa), rownames(cms))
+  
+  refs.merged = merge(aa, y = cms, add.cell.ids = c("Forte2020", "Ren2020"), project = "adultHeart")
+  
+  ref.list <- SplitObject(refs.merged, split.by = "dataset")
+  
+  rm(list = c('aa', 'cms', 'refs.merged')) # remove big seurat objects to clear memory
+  
+  # normalize and identify variable features for each dataset independently
+  ref.list <- lapply(X = ref.list, FUN = function(x) {
+    x <- NormalizeData(x)
+    x <- FindVariableFeatures(x, selection.method = "vst", nfeatures = 3000)
+  })
+  
+  # select features that are repeatedly variable across datasets for integration run PCA on each
+  # dataset using these features
+  features <- SelectIntegrationFeatures(object.list = ref.list, nfeatures = 3000)
+  
+  ref.list <- lapply(X = ref.list, FUN = function(x) {
+    x <- ScaleData(x, features = features.common, verbose = FALSE)
+    x <- RunPCA(x, features = features, verbose = FALSE)
+    
+  })
+  
+  ref.anchors <- FindIntegrationAnchors(object.list = ref.list, anchor.features = features, reduction = "rpca", 
+                                        k.anchor = 5)
+  rm(ref.list)
+  
+  # this command creates an 'integrated' data assay
+  ref.combined <- IntegrateData(anchorset = ref.anchors, features.to.integrate = features.common)
+  
+  # specify that we will perform downstream analysis on the corrected data note that the
+  # original unmodified data still resides in the 'RNA' assay
+  DefaultAssay(ref.combined) <- "integrated"
+  
+  saveRDS(ref.combined, file = paste0(RdataDir, 'Seurat.obj_adultMiceHeart_Forte2020_Ren2020_refCombined_logNormalize_v1.rds'))
+  
+  # Run the standard workflow for visualization and clustering
+  ref.combined <- ScaleData(ref.combined, verbose = FALSE)
+  ref.combined <- RunPCA(ref.combined, npcs = 30, verbose = FALSE)
+  
+  ElbowPlot(ref.combined, ndims = 30)
+  
+  ref.combined <- FindNeighbors(ref.combined, reduction = "pca", dims = 1:20)
+  ref.combined <- FindClusters(ref.combined, resolution = 0.5)
+  
+  ref.combined <- RunUMAP(ref.combined, reduction = "pca", dims = 1:30, n.neighbors = 50, min.dist = 0.05) 
+  
+  
+  # Visualization
+  p1 <- DimPlot(ref.combined, reduction = "umap", group.by = "dataset")
+  p2 <- DimPlot(ref.combined, reduction = "umap", group.by = "annot.ref", label = TRUE,
+                repel = TRUE)
+  p1 + p2 + ggsave(paste0(resDir, '/Forte2020_Ren2020_IntegrationRPCA_', Normalization, '.pdf'), 
+                   width = 16, height = 8)
+  
+  
+}
