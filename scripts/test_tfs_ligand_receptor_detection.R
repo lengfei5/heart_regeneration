529a59535b04bc49c02af382e62229a02b53ca10
jingkui.wang <jingkui.wang@imp.ac.at>
2022-09-20 15:13:41 +0200

    start the test of cell2location

--- scripts/test_tfs_ligand_receptor_detection.R
+++ scripts/test_tfs_ligand_receptor_detection.R
@@ -1,5 +1,5 @@
 ##########################################
-# test the TFs and ligand receptor coverages 
+# TFs and ligand receptor annotations
 ##########################################
 annot = readRDS(paste0('/groups/tanaka/People/current/jiwang/Genomes/axolotl/annotations/', 
                        'geneAnnotation_geneSymbols_cleaning_synteny_sameSymbols.hs.nr_curated.geneSymbol.toUse.rds'))
@@ -26,182 +26,187 @@ annotated_tfs = intersect(tfs, annot$gene.symbol.toUse)
 annotated_ligands = intersect(ligands, annot$gene.symbol.toUse)
 annotated_receptors = intersect(receptors, annot$gene.symbol.toUse)
 
-
-
-ggs = get_geneName(rownames(aa))
-
-## filter lowly expressed and highly expressed genes
-Filtering.lowly.highly.expressed.genes = FALSE
-if(Filtering.lowly.highly.expressed.genes){
-  require(SingleCellExperiment)
-  library(scran)
-  library(scater)
-  library(scuttle)
-  library(Seurat)
-  library(SeuratObject)
+##########################################
+# Elad's snRNA-seq
+##########################################
+Check.TFs.LR.Elad.multiome = FALSE
+if(Check.TFs.LR.Elad.multiome){
+  ggs = get_geneName(rownames(aa))
   
-  sce <- as.SingleCellExperiment(aa)
-  # rm(aa)
+  ## filter lowly expressed and highly expressed genes
+  Filtering.lowly.highly.expressed.genes = FALSE
+  if(Filtering.lowly.highly.expressed.genes){
+    require(SingleCellExperiment)
+    library(scran)
+    library(scater)
+    library(scuttle)
+    library(Seurat)
+    library(SeuratObject)
+    
+    sce <- as.SingleCellExperiment(aa)
+    # rm(aa)
+    
+    fontsize <- theme(axis.text=element_text(size=12), axis.title=element_text(size=16))
+    plotHighestExprs(sce, n=50, exprs_values = "counts")
+    
+    ave.counts <- calculateAverage(sce, assay.type = "counts")
+    
+    hist(log10(ave.counts), breaks=100, main="", col="grey80",
+         xlab=expression(Log[10]~"average count"))
+    
+    num.cells <- nexprs(sce, byrow=TRUE)
+    hist(log10(num.cells), breaks = 100)
+    
+    smoothScatter(log10(ave.counts), num.cells, ylab="Number of cells",
+                  xlab=expression(Log[10]~"average count"))
+    
+    # detected in >= 5 cells, ave.counts >=5 but not too high
+    genes.to.keep <- num.cells > 100 & ave.counts >= 10^-2  & ave.counts <10^1  
+    summary(genes.to.keep)
+    
+    # remove mt and ribo genes
+    #genes.to.keep = genes.to.keep & ! rownames(sce) %in% gg.mt & ! rownames(sce) %in% gg.rb
+    summary(genes.to.keep)
+    
+    sce <- sce[genes.to.keep, ]
+    ave.counts <- calculateAverage(sce, assay.type = "counts")
+    sce = sce[order(ave.counts), ]
+    
+    ggs = get_geneName(rownames(sce))
+    
+  }
   
-  fontsize <- theme(axis.text=element_text(size=12), axis.title=element_text(size=16))
-  plotHighestExprs(sce, n=50, exprs_values = "counts")
   
-  ave.counts <- calculateAverage(sce, assay.type = "counts")
+  expressed_tfs = intersect(tfs, ggs)
+  expressed_ligands = intersect(ligands, ggs)
+  expressed_receptors = intersect(receptors, ggs)
   
-  hist(log10(ave.counts), breaks=100, main="", col="grey80",
-       xlab=expression(Log[10]~"average count"))
+  cat('TFs -- ', length(tfs), '(database)', '-- ', 
+      length(annotated_tfs), ' (annotated in axolotl) --', 
+      length(expressed_tfs), ' annotated in axolotl\n' )
   
-  num.cells <- nexprs(sce, byrow=TRUE)
-  hist(log10(num.cells), breaks = 100)
+  cat('ligands -- ', length(ligands), '(database)', '-- ', 
+      length(annotated_ligands), ' (annotated in axolotl) --', 
+      length(expressed_ligands), ' annotated in axolotl\n' )
   
-  smoothScatter(log10(ave.counts), num.cells, ylab="Number of cells",
-                xlab=expression(Log[10]~"average count"))
+  cat('receptor -- ', length(receptors), '(database)', '-- ', 
+      length(annotated_receptors), ' (annotated in axolotl) --', 
+      length(expressed_receptors), ' annotated in axolotl\n' )
   
-  # detected in >= 5 cells, ave.counts >=5 but not too high
-  genes.to.keep <- num.cells > 100 & ave.counts >= 10^-2  & ave.counts <10^1  
-  summary(genes.to.keep)
+  #lr_network_expressed = lr_network %>% filter(from %in% expressed_ligands & to %in% expressed_receptors) 
+  #head(lr_network_expressed)
   
-  # remove mt and ribo genes
-  #genes.to.keep = genes.to.keep & ! rownames(sce) %in% gg.mt & ! rownames(sce) %in% gg.rb
-  summary(genes.to.keep)
+  res = data.frame(tfs = c(length(tfs), length(annotated_tfs), length(expressed_tfs)),
+                   ligands = c(length(ligands), length(annotated_ligands), length(expressed_ligands)), 
+                   receptors = c(length(receptors), length(annotated_receptors), length(expressed_receptors)))
+  rownames(res) = c('database', 'annotated.axolotl', 'detected.snRNA')
   
-  sce <- sce[genes.to.keep, ]
-  ave.counts <- calculateAverage(sce, assay.type = "counts")
-  sce = sce[order(ave.counts), ]
+  write.csv(res, file = paste0(resDir, '/tfs_ligands_receptors_coverage_snRNA.csv'), 
+            row.names = TRUE)
+  
+  ## double check individual profile of TFs, ligands and receptors
+  ggs = get_geneName(rownames(sce))
+  mm = match(ggs, expressed_tfs)
+  kk = which(!is.na(mm) & ggs != '')
+  
+  
+  subs = sce[kk, ]
+  ave.counts <- calculateAverage(subs, assay.type = "counts")
+  num.cells <- nexprs(subs, byrow=TRUE)
+  
+  res = data.frame(gene = row.names(subs), ave.counts = ave.counts, number.cells = num.cells, 
+                   stringsAsFactors = FALSE)
+  res = res[order(-res$ave.counts), ]
+  
+  write.csv(res, file = paste0(resDir, '/tfs_detected_aveCounts_numCells.csv'), 
+            row.names = TRUE)
+  
+  
+  Idents(aa) = aa$subtypes
+  
+  for(n in 1:length(kk))
+    #for(n in 1:10)
+  {
+    # n = 1
+    cat(n, '\n')
+    p1 = FeaturePlot(aa, features = rownames(sce)[kk[n]])
+    p2 = VlnPlot(aa, features = rownames(sce)[kk[n]]) + NoLegend()
+    
+    p1 + p2
+    
+    ggsave(filename = paste0(resDir, '/tfs/CoverageTest_', rownames(sce)[kk[n]], '.pdf'),
+           width = 10, height = 16)
+    
+  }
+  
   
   ggs = get_geneName(rownames(sce))
+  mm = match(ggs, expressed_ligands)
+  kk = which(!is.na(mm) & ggs != '')
   
-}
-
-
-expressed_tfs = intersect(tfs, ggs)
-expressed_ligands = intersect(ligands, ggs)
-expressed_receptors = intersect(receptors, ggs)
-
-cat('TFs -- ', length(tfs), '(database)', '-- ', 
-    length(annotated_tfs), ' (annotated in axolotl) --', 
-    length(expressed_tfs), ' annotated in axolotl\n' )
-
-cat('ligands -- ', length(ligands), '(database)', '-- ', 
-    length(annotated_ligands), ' (annotated in axolotl) --', 
-    length(expressed_ligands), ' annotated in axolotl\n' )
-
-cat('receptor -- ', length(receptors), '(database)', '-- ', 
-    length(annotated_receptors), ' (annotated in axolotl) --', 
-    length(expressed_receptors), ' annotated in axolotl\n' )
-
-#lr_network_expressed = lr_network %>% filter(from %in% expressed_ligands & to %in% expressed_receptors) 
-#head(lr_network_expressed)
-
-res = data.frame(tfs = c(length(tfs), length(annotated_tfs), length(expressed_tfs)),
-                 ligands = c(length(ligands), length(annotated_ligands), length(expressed_ligands)), 
-                 receptors = c(length(receptors), length(annotated_receptors), length(expressed_receptors)))
-rownames(res) = c('database', 'annotated.axolotl', 'detected.snRNA')
-
-write.csv(res, file = paste0(resDir, '/tfs_ligands_receptors_coverage_snRNA.csv'), 
-          row.names = TRUE)
-
-## double check individual profile of TFs, ligands and receptors
-ggs = get_geneName(rownames(sce))
-mm = match(ggs, expressed_tfs)
-kk = which(!is.na(mm) & ggs != '')
-
-
-subs = sce[kk, ]
-ave.counts <- calculateAverage(subs, assay.type = "counts")
-num.cells <- nexprs(subs, byrow=TRUE)
-
-res = data.frame(gene = row.names(subs), ave.counts = ave.counts, number.cells = num.cells, 
-                 stringsAsFactors = FALSE)
-res = res[order(-res$ave.counts), ]
-
-write.csv(res, file = paste0(resDir, '/tfs_detected_aveCounts_numCells.csv'), 
-          row.names = TRUE)
-
-
-Idents(aa) = aa$subtypes
-
-for(n in 1:length(kk))
-#for(n in 1:10)
-{
-  # n = 1
-  cat(n, '\n')
-  p1 = FeaturePlot(aa, features = rownames(sce)[kk[n]])
-  p2 = VlnPlot(aa, features = rownames(sce)[kk[n]]) + NoLegend()
+  subs = sce[kk, ]
+  ave.counts <- calculateAverage(subs, assay.type = "counts")
+  num.cells <- nexprs(subs, byrow=TRUE)
   
-  p1 + p2
+  res = data.frame(gene = row.names(subs), ave.counts = ave.counts, number.cells = num.cells, 
+                   stringsAsFactors = FALSE)
+  res = res[order(-res$ave.counts), ]
   
-  ggsave(filename = paste0(resDir, '/tfs/CoverageTest_', rownames(sce)[kk[n]], '.pdf'),
-         width = 10, height = 16)
+  write.csv(res, file = paste0(resDir, '/ligands_detected_aveCounts_numCells.csv'), 
+            row.names = TRUE)
   
-}
-
-
-ggs = get_geneName(rownames(sce))
-mm = match(ggs, expressed_ligands)
-kk = which(!is.na(mm) & ggs != '')
-
-subs = sce[kk, ]
-ave.counts <- calculateAverage(subs, assay.type = "counts")
-num.cells <- nexprs(subs, byrow=TRUE)
-
-res = data.frame(gene = row.names(subs), ave.counts = ave.counts, number.cells = num.cells, 
-                 stringsAsFactors = FALSE)
-res = res[order(-res$ave.counts), ]
-
-write.csv(res, file = paste0(resDir, '/ligands_detected_aveCounts_numCells.csv'), 
-          row.names = TRUE)
-
-ggs = get_geneName(rownames(sce))
-kk = which(!is.na(match(ggs, expressed_ligands)))
-Idents(aa) = aa$subtypes
-
-for(n in 1:length(kk))
-#for(n in 1:10)
-{
-  # n = 1
-  cat(n, '\n')
-  p1 = FeaturePlot(aa, features = rownames(sce)[kk[n]])
-  p2 = VlnPlot(aa, features = rownames(sce)[kk[n]]) + NoLegend()
+  ggs = get_geneName(rownames(sce))
+  kk = which(!is.na(match(ggs, expressed_ligands)))
+  Idents(aa) = aa$subtypes
   
-  p1 + p2
+  for(n in 1:length(kk))
+    #for(n in 1:10)
+  {
+    # n = 1
+    cat(n, '\n')
+    p1 = FeaturePlot(aa, features = rownames(sce)[kk[n]])
+    p2 = VlnPlot(aa, features = rownames(sce)[kk[n]]) + NoLegend()
+    
+    p1 + p2
+    
+    ggsave(filename = paste0(resDir, '/ligands/CoverageTest_', rownames(sce)[kk[n]], '.pdf'),
+           width = 10, height = 16)
+    
+  }
   
-  ggsave(filename = paste0(resDir, '/ligands/CoverageTest_', rownames(sce)[kk[n]], '.pdf'),
-         width = 10, height = 16)
+  ggs = get_geneName(rownames(sce))
+  mm = match(ggs, expressed_receptors)
+  kk = which(!is.na(mm) & ggs != '')
   
-}
-
-ggs = get_geneName(rownames(sce))
-mm = match(ggs, expressed_receptors)
-kk = which(!is.na(mm) & ggs != '')
-
-subs = sce[kk, ]
-ave.counts <- calculateAverage(subs, assay.type = "counts")
-num.cells <- nexprs(subs, byrow=TRUE)
-
-res = data.frame(gene = row.names(subs), ave.counts = ave.counts, number.cells = num.cells, 
-                 stringsAsFactors = FALSE)
-res = res[order(-res$ave.counts), ]
-
-write.csv(res, file = paste0(resDir, '/receptors_detected_aveCounts_numCells.csv'), 
-          row.names = TRUE)
-
-
-kk = which(!is.na(match(ggs, expressed_receptors)))
-Idents(aa) = aa$subtypes
-
-for(n in 1:length(kk))
-  #for(n in 1:10)
-{
-  # n = 1
-  cat(n, '\n')
-  p1 = FeaturePlot(aa, features = rownames(sce)[kk[n]])
-  p2 = VlnPlot(aa, features = rownames(sce)[kk[n]]) + NoLegend()
+  subs = sce[kk, ]
+  ave.counts <- calculateAverage(subs, assay.type = "counts")
+  num.cells <- nexprs(subs, byrow=TRUE)
   
-  p1 + p2
+  res = data.frame(gene = row.names(subs), ave.counts = ave.counts, number.cells = num.cells, 
+                   stringsAsFactors = FALSE)
+  res = res[order(-res$ave.counts), ]
   
-  ggsave(filename = paste0(resDir, '/receptors/CoverageTest_', rownames(sce)[kk[n]], '.pdf'),
-         width = 10, height = 16)
+  write.csv(res, file = paste0(resDir, '/receptors_detected_aveCounts_numCells.csv'), 
+            row.names = TRUE)
+  
+  
+  kk = which(!is.na(match(ggs, expressed_receptors)))
+  Idents(aa) = aa$subtypes
+  
+  for(n in 1:length(kk))
+    #for(n in 1:10)
+  {
+    # n = 1
+    cat(n, '\n')
+    p1 = FeaturePlot(aa, features = rownames(sce)[kk[n]])
+    p2 = VlnPlot(aa, features = rownames(sce)[kk[n]]) + NoLegend()
+    
+    p1 + p2
+    
+    ggsave(filename = paste0(resDir, '/receptors/CoverageTest_', rownames(sce)[kk[n]], '.pdf'),
+           width = 10, height = 16)
+    
+  }
   
 }
 
@@ -230,7 +235,8 @@ if(Filtering.lowly.highly.expressed.genes){
   library(Seurat)
   library(SeuratObject)
   
-  sce <- as.SingleCellExperiment(srat_mtm)
+  #sce <- as.SingleCellExperiment(srat_mtm)
+  sce <- as.SingleCellExperiment(srat_sn)
   ggs = rownames(sce)
   # rm(aa)
   
@@ -288,9 +294,14 @@ cat('receptor -- ', length(receptors), '(database)', '-- ',
 res = data.frame(tfs = c(length(tfs), length(annotated_tfs), length(expressed_tfs)),
                  ligands = c(length(ligands), length(annotated_ligands), length(expressed_ligands)), 
                  receptors = c(length(receptors), length(annotated_receptors), length(expressed_receptors)))
-rownames(res) = c('database', 'annotated.axolotl', 'detected.snRNA')
+rownames(res) = c('database', 'annotated.axolotl', 'detected.multiome')
 
-write.csv(res, file = paste0(resDir, '/tfs_ligands_receptors_coverage_snRNA_LustBrain.csv'), 
+res = t(res)
+
+res = cbind(res, c(length(expressed_tfs), length(expressed_ligands), length(expressed_receptors)))
+colnames(res)[ncol(res)] = 'detected.sn'
+
+write.csv(res, file = paste0(resDir, '/tfs_ligands_receptors_coverage_multiome.vs.snRNA_LustBrain.csv'), 
           row.names = TRUE)
 
 
@@ -364,6 +375,7 @@ res = res[order(-res$ave.counts_multiome), ]
 
 write.csv(res, file = paste0(resDir, '/tfs_ligands_receptors_detected_aveCounts_numCells_LustBrain.csv'), 
           row.names = TRUE)
+
 
 for(n in 1:nrow(res))
 #for(n in 1:10)
