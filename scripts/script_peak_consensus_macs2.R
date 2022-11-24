0bd211ee8f13e270427e8e5e119e9dd769540b61
jingkui.wang <jingkui.wang@imp.ac.at>
2022-11-24 09:38:29 +0100

    mv the macs2 peak test into test script and also rm analysis_RCTD_result.R

--- scripts/script_peak_consensus_macs2.R
+++ scripts/script_peak_consensus_macs2.R
@@ -324,4 +324,165 @@ saveRDS(peaks, file = paste0(RdataDir, 'merged_peaks_macs2_secondRound_800K_pval
 
 
 
+########################################################
+########################################################
+# Section II: 
+# 1) requantify the count table using the peak consensus by macs2
+# 2) check QCs for individual sample from the cellranger-arc output
+########################################################
+########################################################
+# check the current active plan
+#plan()
+#plan("multicore", workers = 1)
+#plan()
+
+#combined.peaks = readRDS(file = paste0(RdataDir, 'merged_peaks_macs2_secondRound_290K.rds'))
+combined.peaks = readRDS(file = paste0(RdataDir, 'merged_peaks_macs2_secondRound_800K_pval5.rds'))
+
+# Filter out bad peaks based on length
+peakwidths = width(combined.peaks)
+combined.peaks = combined.peaks[peakwidths > 50]
+# there is a problem with coordinates starting at 0 for some reason...
+combined.peaks = restrict(combined.peaks, start = 1)
+
+length(combined.peaks)
+combined.peaks
+
+srat = list()
+for(n in 1:nrow(design))
+{
+  # n = 1
+  cat('----------- : ', n, ':',  design$condition[n], '-------------\n')
+  
+  # load nf output and process
+  topdir = paste0(dataDir, '/multiome_', design$timepoint[n], '/outs')
+  counts <- Read10X_h5(filename = paste0(topdir, "/filtered_feature_bc_matrix.h5"))
+  fragpath <- paste0(topdir, "/atac_fragments.tsv.gz")
+  frags_l = CreateFragmentObject(path = fragpath, cells = colnames(counts$Peaks))
+  
+  # slow step takes 16 mins without parall computation
+  tic()
+  feat = FeatureMatrix(fragments = frags_l, features = combined.peaks, 
+                       cells = colnames(counts$Peaks))
+  toc()
+  
+  chrom_assay <- CreateChromatinAssay(
+    counts = feat,
+    sep = c(":", "-"),
+    fragments = frags_l,
+    annotation = granges_axolotl,
+    min.cells = 0,
+    min.features = 0 
+  )
+  
+  bb <- CreateSeuratObject(
+    counts = chrom_assay,
+    assay = "ATAC",
+    
+  )
+  rm(chrom_assay)
+  
+  metadata <- read.csv(
+    file = paste0(topdir, '/per_barcode_metrics.csv'),
+    header = TRUE,
+    row.names = 1
+  )
+  
+  bb$condition = design$condition[n]
+  
+  mm = match(colnames(bb), metadata$gex_barcode)
+  metadata = metadata[, c(1,2, 3, 21:30)]
+  
+  bb = AddMetaData(bb, metadata = metadata)
+  
+  bb$pct_reads_in_peaks <- bb$atac_peak_region_fragments / bb$atac_fragments * 100
+  bb$pct_usable_fragments = bb$atac_fragments/bb$atac_raw_reads
+  
+  DefaultAssay(bb) <- "ATAC"
+  
+  srat[[n]] = bb
+  
+}
+
+saveRDS(srat, file = (paste0(RdataDir, 'seuratObj_scATAC_merged.peaks.macs2.800K.rds')))
+
+srat = Reduce(merge, srat)
+
+saveRDS(srat, file = (paste0(RdataDir, 'seuratObj_scATAC_merged.peaks.macs2.800K_v2.rds')))
+
+
+##########################################
+# QCs and filtering 
+##########################################
+#srat = readRDS(file = (paste0(RdataDir, 'seuratObj_scATAC_merged.peaks.macs2.290K.rds')))
+srat = readRDS(file = (paste0(RdataDir, 'seuratObj_scATAC_merged.peaks.macs2.800K_v2.rds')))
+levels = design$condition
+srat$condition = factor(srat$condition, levels = levels)
+Idents(srat) = srat$condition
+
+srat <- NucleosomeSignal(srat)
+
+srat <- TSSEnrichment(srat, fast = FALSE)
+
+srat$high.tss <- ifelse(srat$TSS.enrichment > 5, 'High', 'Low')
+
+saveRDS(srat, file = (paste0(RdataDir, 'seuratObj_scATAC_merged.peaks.macs2.800K_v2_QCs.rds')))
+
+
+TSSPlot(srat, group.by = 'high.tss') + NoLegend()
+
+VlnPlot(srat, features = "nCount_ATAC", ncol = 1, y.max = 5000, group.by = 'condition', pt.size = 0.) +
+  geom_hline(yintercept = c(200, 500, 1000))
+
+ggsave(filename = paste0(resDir, '/QCs_nCount_ATAC_macsPeaks.pdf'), height =8, width = 12 )
+
+VlnPlot(srat, features = c("atac_fragments"), y.max = 10^5)
+ggsave(filename = paste0(resDir, '/QCs_atac_fragments_macsPeaks.pdf'), height =8, width = 12 )
+
+VlnPlot(srat, features = c("pct_reads_in_peaks"))
+ggsave(filename = paste0(resDir, '/QCs_pct_readsWithinPeaks_macsPeaks.pdf'), height =8, width = 12 )
+
+VlnPlot(object = srat, features = c("TSS.enrichment"), pt.size = 0, y.max = 10) +
+  geom_hline(yintercept = c(1, 2))
+ggsave(filename = paste0(resDir, '/QCs_TSS.enrichment_macsPeaks.pdf'), height =8, width = 12 )
+
+VlnPlot(object = srat, features = c("nucleosome_signal"), pt.size = 0)
+ggsave(filename = paste0(resDir, '/QCs_nucleosome_signal_macsPeaks.pdf'), height =8, width = 12 )
+
+# quick filtering 
+srat <- subset(
+  x = srat,
+  subset = nCount_ATAC > 100 &
+    nCount_ATAC < 20000 &
+    #pct_reads_in_peaks > 15 &
+    #blacklist_ratio < 0.05 &
+    nucleosome_signal < 4 &
+    TSS.enrichment > 1
+)
+
+srat = RunTFIDF(srat)
+srat = FindTopFeatures(srat, min.cutoff = 'q5')
+srat = RunSVD(srat)
+
+DepthCor(srat, n = 30)
+
+#cordat = DepthCor(srat, n = 30)$data
+cordat = DepthCor(srat, reduction = "lsi", n = 30)$data
+dims_use = cordat$Component[abs(cordat$counts)<0.3]
+
+#dims_use = c(2:30)
+print(dims_use)
+
+srat = FindNeighbors(object = srat, reduction = 'lsi', dims = dims_use, 
+                     force.recalc = T, graph.name = "thegraph")
+srat = FindClusters(object = srat, verbose = FALSE, algorithm = 3, 
+                    graph.name = "thegraph", resolution = 1)
+
+srat <- RunUMAP(object = srat, reduction = 'lsi', dims = dims_use, n.neighbors = 30, min.dist = 0.1)
+
+DimPlot(object = srat, label = TRUE, repel = TRUE) + NoLegend()
+ggsave(filename = paste0(resDir, '/macsPeaks_umap_v1.pdf'), height =8, width = 12 )
+
+DimPlot(object = srat, label = TRUE, repel = TRUE, split.by = 'condition') + NoLegend()
+ggsave(filename = paste0(resDir, '/macsPeaks_umap_perCondition_v1.pdf'), height =8, width = 30 )
 
