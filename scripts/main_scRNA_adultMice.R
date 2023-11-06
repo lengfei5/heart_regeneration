1ba85da32123271f919075f8c5ae0013c50f51f9
jingkui.wang <jingkui.wang@imp.ac.at>
2023-11-06 12:01:33 +0100

    change the file names by specifying mice

---
+++ scripts/main_scRNA_adultMice.R
@@ -0,0 +1,10 @@
+##########################################################################
+##########################################################################
+# Project: Heart regeneration 
+# Script purpose: process and analyze published sc/snRNA-seq data in adult mouse 
+# Usage example: 
+# Author: Jingkui Wang (jingkui.wang@imp.ac.at)
+# Date of creation: Mon Nov  6 11:59:33 2023
+##########################################################################
+##########################################################################
+
