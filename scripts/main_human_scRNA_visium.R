29a7759c6a1b840ead7e20fa9f4fc0da1930de00
jingkui.wang <jingkui.wang@imp.ac.at>
2024-03-12 15:28:08 +0100

    save the scRNA-seq systematic annotation

--- scripts/main_human_scRNA_visium.R
+++ scripts/main_human_scRNA_visium.R
@@ -7,7 +7,6 @@
 # Date of creation: Thu Feb 22 11:34:57 2024
 ##########################################################################
 ##########################################################################
-
 rm(list = ls())
 
 version.analysis = '_20231207'
@@ -34,7 +33,6 @@ options(future.globals.maxSize = 160000 * 1024^2)
 library(pryr) # monitor the memory usage
 
 mem_used()
-
 Normalization = 'lognormal' # ('lognormal or SCT')
 
 dataDir = "../published_dataset/human/Kuppe_et_al_2022/processed_data_Robj/snRNA/"
@@ -92,7 +90,6 @@ ggsave(paste0(resDir, '/Kupper2022_Umap_clusters_cellType.original_selectedDonor
 
 saveRDS(aa, file = paste0(RdataDir, '/Kuppe2022_heart_donorSelected.rds'))
 
-
 ########################################################
 ########################################################
 # Section II: # add subtypes with the original subclusterd based on snRNA and scATAC
@@ -100,6 +97,18 @@ saveRDS(aa, file = paste0(RdataDir, '/Kuppe2022_heart_donorSelected.rds'))
 # 
 ########################################################
 ########################################################
+#library(org.Mm.eg.db)
+library('rtracklayer')
+library(GenomicRanges)
+library('GenomicFeatures')
+
+annot = import(paste0('/groups/tanaka/People/current/jiwang/Genomes/human/hg38/annotation/', 
+                      'Homo_sapiens.GRCh38.111.gtf'))
+
+annot = data.frame(id = annot$gene_id, symbol = annot$gene_name, type = annot$gene_biotype)
+annot = annot[which(annot$type == 'protein_coding'), ]
+annot = annot[match(unique(annot$id), annot$id), ]
+
 aa = readRDS(file = paste0(RdataDir, '/Kuppe2022_heart_donorSelected.rds'))
 aa$patient_region_id = droplevels(aa$patient_region_id)
 
@@ -115,45 +124,113 @@ annot_file = list.files(path = '../published_dataset/human/Kuppe_et_al_2022/proc
 
 aa$annotation = NA
 
+
 ##########################################
 # subtypes of CMs
 ##########################################
-for(n in 1:length(annot_file))
+for(n in 3:length(annot_file))
 {
-  # n = 1
+  # n = 2
   celltype = gsub('_snRNA_snATAC.Rds', '', basename(annot_file[n]))
-  cat(n, '--', basename(annot_file[n]), '\n')
+  
+  outDir = paste0(resDir, '/', celltype)
+  if(!dir.exists(outDir)) dir.create(outDir)
+  
+  cat(n, '--', basename(annot_file[n]), '-- cell type : ', celltype, '\n')
   xx = readRDS(annot_file[n])
   cat(nrow(xx), 'cells \n')
   print(table(xx$annotation))
   
-  xx$cell.id = sapply(colnames(xx), function(x) {unlist(strsplit(x, '#'))[2]})
-  xx$cell.id = paste0(xx$patient_region_id, '_', xx$cell.id)
+  #xx$cell.id = sapply(colnames(xx), function(x) {unlist(strsplit(x, '#'))[2]})
+  #xx$cell.id = paste0(xx$patient_region_id, '_', xx$cell.id)
   
   xx = DietSeurat(xx, data = TRUE, assays = 'RNA')
   
   subs = subset(aa, cells = colnames(aa)[which(aa$cell_type_original == celltype)])
-  DimPlot(subs, group.by = 'cell_type_original', raster=FALSE, label = TRUE, repel = TRUE)
-    
-  #mm = match(xx$cell.id, aa$cell.id)
-  #cat(length(which(!is.na(mm))), 'cell matches \n')
-  subs <- FindVariableFeatures(aa, selection.method = 'vst', nfeatures = 2000)
-  #subs <- ScaleData(subs)
-  #subs <- RunPCA(subs, features = VariableFeatures(object = subs), verbose = FALSE, weight.by.var = FALSE)
   
+  DimPlot(subs, group.by = 'cell_type_original', raster=FALSE, label = TRUE, repel = TRUE)
+  
+  subs <- FindVariableFeatures(subs, selection.method = 'vst', nfeatures = 2000)
+    
   #ElbowPlot(subs, ndims = 50)
   subs <- FindNeighbors(subs, dims = 1:20, reduction = 'harmony')
-  subs <- FindClusters(subs, verbose = FALSE, algorithm = 3, resolution = 0.7)
+  subs <- FindClusters(subs, verbose = FALSE, algorithm = 3, resolution = 1.0)
   
-  #Idents(subs) = subs$condition
   subs <- RunUMAP(subs, dims = 1:20, reduction = 'harmony', n.neighbors = 30, min.dist = 0.3)
   
-  DimPlot(subs, label = TRUE, repel = TRUE, group.by = 'seurat_clusters', raster=FALSE)
+  p1 = DimPlot(subs, label = TRUE, repel = TRUE, group.by = 'cell_type_original', raster=FALSE)
+  p2 =  DimPlot(subs, label = TRUE, repel = TRUE, group.by = 'seurat_clusters', raster=FALSE)
   
-  ggsave(filename = paste0(resDir, '/umap_timepoints_clusters_regress.nCounts.pdf'), width = 16, height = 6)
+  p1 + p2 
+  ggsave(filename = paste0(outDir, '/umap_clusters_celltype_', celltype, '.pdf'), width = 16, height = 8)
   
+  # pseudo-bulk by per donor per cell type
+  pb <- AverageExpression(subs, return.seurat = TRUE, slot = 'counts', features = VariableFeatures(subs),
+                          group.by = c("seurat_clusters"))
+  clusters = pb@assays$RNA@data
+  rm(pb)
   
+  ref = AverageExpression(xx, return.seurat = TRUE, slot = 'counts',
+                          group.by = c("annotation"))
+  ref = ref@assays$RNA@data
+  
+  ggs = annot$symbol[match(rownames(clusters), annot$id)]
+  
+  gg_sels = unique(ggs[which(!is.na(ggs))])
+  clusters = clusters[match(gg_sels, ggs), ]
+  rownames(clusters) = gg_sels
+  rm(ggs)
+  
+  ggs = intersect(rownames(clusters), rownames(ref))
+  clusters = clusters[match(ggs, rownames(clusters)), ]
+  ref = ref[match(ggs, rownames(ref)), ]
+  
+  keep = matrix(NA, nrow = ncol(clusters), ncol = 4)
+  rownames(keep) = colnames(clusters)
+  colnames(keep) = c('cluster.pearson', 'cluster.spearman',  
+                     'cor.pearson', 'cor.spearman')
+  
+  for(m in 1:ncol(clusters))
+  {
+    # m = 1
+    cat(m, ' -- ', colnames(clusters)[m], '\n')
+    cors = cor(clusters[,m], ref, use = 'everything', method = 'pearson')
+    keep[m, 1] = colnames(ref)[which.max(cors)]
+    keep[m, 3] = cors[which.max(cors)]
     
+    cors = cor(clusters[,m], ref, use = 'everything', method = 'spearman')
+    keep[m, 2] = colnames(ref)[which.max(cors)]
+    keep[m, 4] = cors[which.max(cors)]
+    
+  }
+  
+  keep = data.frame(keep)
+  
+  for(m in 1:nrow(keep))
+  {
+    # m = 1
+    if(as.numeric(keep$cor.spearman[m]) > 0.7 & keep$cluster.pearson[m] == keep$cluster.spearman[m]){
+      cat(rownames(keep)[m], '\n')
+      cells = colnames(subs)[which(subs$seurat_clusters == rownames(keep)[m])]
+      aa$annotation[match(cells, colnames(aa))] = keep$cluster.spearman[m]
+    }
+    
+  }
+  
+  #DimPlot(aa, reduction = 'umap', group.by = 'cell_type_original', raster=FALSE, label = TRUE, repel = TRUE)
+  #ggsave(filename = paste0(outDir, '/umap_celltype.pdf'), width = 12, height = 8)
+  
+  DimPlot(aa, reduction = 'umap', group.by = 'annotation', raster=FALSE, label = TRUE, repel = TRUE)
+  ggsave(filename = paste0(outDir, '/umap_subtypes_for_celltype_', celltype, '.pdf'), width = 12, height = 8)
+  
+  saveRDS(aa, file = paste0(RdataDir, '/Kuppe2022_heart_donorSelected_subtypes_', celltype, '.rds'))
+  
 }
+
+saveRDS(aa, file = paste0(RdataDir, '/Kuppe2022_heart_donorSelected_subtypes.added.rds'))
+
+
+
+
 
 
