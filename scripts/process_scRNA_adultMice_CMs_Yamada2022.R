ebcb3119d56cefde268c95f571fcbd6f0b38b53f
jingkui.wang <jingkui.wang@imp.ac.at>
2026-07-17 11:39:54 +0200

    save and continue with the CM analysis

--- scripts/process_scRNA_adultMice_CMs_Yamada2022.R
+++ scripts/process_scRNA_adultMice_CMs_Yamada2022.R
@@ -36,6 +36,12 @@ library(gridExtra)
 library(ggrepel)
 library(reshape2)
 
+library(dplyr)
+library(Seurat)
+library(patchwork)
+library(DropletUtils)
+
+sessionInfo()
 
 source('functions_scRNAseq.R')
 source('functions_Visium.R')
@@ -51,64 +57,147 @@ dataDir = "../published_dataset/adult_mice/Yamada_2022/GSE176092_RAW/"
 # original code from https://github.com/firstheart123/spatiotemporal_heart/blob/main/snRNA_seq.R
 ########################################################
 ########################################################
-library(dplyr)
-library(Seurat)
-library(patchwork)
-
-sessionInfo()
-
-name_list <- c("MI_D1_IR_1", "MI_D1_IR_2", "MI_D1_RR_1", "MI_D1_RR_2",
-               "MI_D7_IR_1", "MI_D7_IR_2", "MI_D7_RR_1", "MI_D7_RR_2",
-               "MI_D14_IR_1", "MI_D14_IR_2", "MI_D14_RR_1", "MI_D14_RR_2",
-               "Sham_1", "Sham_2")
-
-data_1 <- Read10X("WT_MI_D1_IR_1/outs/filtered_feature_bc_matrix") %>% CreateSeuratObject(project = name_list[1], min.cells = 0, min.features = 0) %>% RenameCells(add.cell.id = name_list[1])
-data_2 <- Read10X("WT_MI_D1_IR_2/outs/filtered_feature_bc_matrix") %>% CreateSeuratObject(project = name_list[2], min.cells = 0, min.features = 0) %>% RenameCells(add.cell.id = name_list[2])
-data_3 <- Read10X("WT_MI_D1_RR_1/outs/filtered_feature_bc_matrix") %>% CreateSeuratObject(project = name_list[3], min.cells = 0, min.features = 0) %>% RenameCells(add.cell.id = name_list[3])
-data_4 <- Read10X("WT_MI_D1_RR_2/outs/filtered_feature_bc_matrix") %>% CreateSeuratObject(project = name_list[4], min.cells = 0, min.features = 0) %>% RenameCells(add.cell.id = name_list[4])
-data_5 <- Read10X("WT_MI_D7_IR_1/outs/filtered_feature_bc_matrix") %>% CreateSeuratObject(project = name_list[5], min.cells = 0, min.features = 0) %>% RenameCells(add.cell.id = name_list[5])
-data_6 <- Read10X("WT_MI_D7_IR_2/outs/filtered_feature_bc_matrix") %>% CreateSeuratObject(project = name_list[6], min.cells = 0, min.features = 0) %>% RenameCells(add.cell.id = name_list[6])
-data_7 <- Read10X("WT_MI_D7_RR_1/outs/filtered_feature_bc_matrix") %>% CreateSeuratObject(project = name_list[7], min.cells = 0, min.features = 0) %>% RenameCells(add.cell.id = name_list[7])
-data_8 <- Read10X("WT_MI_D7_RR_2/outs/filtered_feature_bc_matrix") %>% CreateSeuratObject(project = name_list[8], min.cells = 0, min.features = 0) %>% RenameCells(add.cell.id = name_list[8])
-data_9 <- Read10X("WT_MI_D14_IR_1/outs/filtered_feature_bc_matrix") %>% CreateSeuratObject(project = name_list[9], min.cells = 0, min.features = 0) %>% RenameCells(add.cell.id = name_list[9])
-data_10 <- Read10X("WT_MI_D14_IR_2/outs/filtered_feature_bc_matrix") %>% CreateSeuratObject(project = name_list[10], min.cells = 0, min.features = 0) %>% RenameCells(add.cell.id = name_list[10])
-data_11 <- Read10X("WT_MI_D14_RR_1/outs/filtered_feature_bc_matrix") %>% CreateSeuratObject(project = name_list[11], min.cells = 0, min.features = 0) %>% RenameCells(add.cell.id = name_list[11])
-data_12 <- Read10X("WT_MI_D14_RR_2/outs/filtered_feature_bc_matrix") %>% CreateSeuratObject(project = name_list[12], min.cells = 0, min.features = 0) %>% RenameCells(add.cell.id = name_list[12])
-data_13 <- Read10X("WT_Sham_1/outs/filtered_feature_bc_matrix") %>% CreateSeuratObject(project = name_list[13], min.cells = 0, min.features = 0) %>% RenameCells(add.cell.id = name_list[13])
-data_14 <- Read10X("WT_Sham_2/outs/filtered_feature_bc_matrix") %>% CreateSeuratObject(project = name_list[14], min.cells = 0, min.features = 0) %>% RenameCells(add.cell.id = name_list[14])
-
-all <- merge(data_1, c(data_2, data_3, data_4, data_5, data_6, data_7, data_8, data_9, data_10, data_11, data_12, data_13, data_14))
+file_list = list.files(path = dataDir, all.files = TRUE, full.names = FALSE, no.. = TRUE)
 
 
-all[["percent.mt"]] <- PercentageFeatureSet(all, pattern = "^mt-")
+samples = gsub('_barcodes.tsv.gz', '', file_list)
+samples = gsub('_features.tsv.gz', '', samples)
+samples = gsub('_matrix.mtx.gz|.tif.gz|_tissue_positions_list.csv.gz', '', samples)
+samples = unique(samples)
+samples = samples[grep('_CM_', samples)]
 
-mt.index <- grep(pattern = "^mt-", x = rownames(all[["RNA"]]), value = FALSE)
-all_matrix <- all[["RNA"]][-mt.index, ]
-all <- CreateSeuratObject(counts = all_matrix, meta.data = all@meta.data)
 
-all_list <- SplitObject(all, split.by = "orig.ident")
+# import data from cellranger output
+for(n in 1:length(samples))
+{
+  # n = 1
+  cat(n, ' : ', samples[n], '\n')
+  
+  topdir = paste0(dataDir)
+  
+  exp = Matrix::readMM(paste0(topdir, samples[n], "_matrix.mtx.gz")) #read matrix
+  bc = read.csv(paste0(topdir, samples[n], "_barcodes.tsv.gz"), header = F, stringsAsFactors = F)
+  g = read.csv(paste0(topdir, samples[n], "_features.tsv.gz"), header = F, stringsAsFactors = F, sep = '\t')
+  
+  ## make unique gene names
+  g$name = g$V2
+  gg.counts = table(g$V2)
+  gg.dup = names(gg.counts)[which(gg.counts>1)]
+  index.dup = which(!is.na(match(g$V2, gg.dup)))
+  g$name[index.dup] = paste0(g$V2[index.dup], '_', g$V1[index.dup])
+  
+  colnames(exp) = bc$V1
+  rownames(exp) = g$name
+  
+  count.data = exp
+  rm(exp);
+  
+  cat('get empty drops with UMI rank \n')
+  
+  # get emptyDrops and default cutoff cell estimates
+  iscell_dd = defaultDrops(count.data, expected = 8000) # default cell estimate, similar to 10x cellranger
+  sum(iscell_dd, na.rm=TRUE)
+  
+  ## not used the emptyDrops too slow 
+  # eout = emptyDrops(count.data, lower = 200)
+  # eout$FDR[is.na(eout$FDR)] = 1
+  # iscell_ed = eout$FDR<=0.01
+  # sum(iscell_ed, na.rm=TRUE)
+  
+  meta = data.frame(row.names = colnames(count.data), condition = samples[n],
+                    iscell_dd = iscell_dd)
+  
+  # use defaultDrop to select cells.
+  aa = CreateSeuratObject(counts = count.data[, iscell_dd],
+                          meta.data = meta[iscell_dd, ], 
+                          min.cells = 0, min.features = 0)
+  aa$cell.id = paste0(samples[n], '_', colnames(aa))
+  
+  if(n == 1) {
+    mnt = aa
+  }else{
+    mnt = merge(mnt, aa)
+  }
+}
 
-all_list <- lapply(X = all_list, FUN = function(x) {
+#mnt[["percent.mt"]] <- PercentageFeatureSet(mnt, pattern = "^mt-")
+
+saveRDS(mnt, file = paste0(RdataDir, 'seuratObject_',  version.analysis, '.rds'))
+
+
+##########################################
+# process the data
+##########################################
+aa = readRDS(file = paste0(RdataDir, 'seuratObject_',  version.analysis, '.rds'))
+
+#aa = mnt 
+#rm(mnt)
+
+#images = file_list[grep('tif', file_list)]
+aa$condition[grep('CM_Sham_1', aa$condition)] = 'Sham_1'
+aa$condition[grep('CM_Sham_2', aa$condition)] = 'Sham_2'
+
+aa$condition[grep('CM_MI_day1_IZ\\+BZ_1', aa$condition)] = 'MI_day1_IR_1'
+aa$condition[grep('CM_MI_day1_IZ\\+BZ_2', aa$condition)] = 'MI_day1_IR_2'
+aa$condition[grep('CM_MI_day1_RZ_1', aa$condition)] = 'MI_day1_RR_1'
+aa$condition[grep('CM_MI_day1_RZ_2', aa$condition)] = 'MI_day1_RR_2'
+
+aa$condition[grep('CM_MI_day7_IZ\\+BZ_1', aa$condition)] = 'MI_day7_IR_1'
+aa$condition[grep('CM_day7_IZ\\+BZ_2', aa$condition)] = 'MI_day7_IR_2'
+aa$condition[grep('CM_MI_day7_RZ_1', aa$condition)] = 'MI_day7_RR_1'
+aa$condition[grep('CM_MI_day7_RZ_2', aa$condition)] = 'MI_day7_RR_2'
+
+aa$condition[grep('CM_MI_day14_IZ\\+BZ_1', aa$condition)] = 'MI_day14_IR_1'
+aa$condition[grep('CM_MI_day14_IZ\\+BZ_2', aa$condition)] = 'MI_day14_IR_2'
+aa$condition[grep('CM_MI_day14_RZ_1', aa$condition)] = 'MI_day14_RR_1'
+aa$condition[grep('CM_MI_day14_RZ_2', aa$condition)] = 'MI_day14_RR_2'
+
+
+aa[["percent.mt"]] <- PercentageFeatureSet(aa, pattern = "^mt-")
+
+mt.index <- grep(pattern = "^mt-", x = rownames(aa[["RNA"]]), value = FALSE)
+aa_matrix <- aa[["RNA"]][-mt.index, ]
+aa <- CreateSeuratObject(counts = aa_matrix, meta.data = aa@meta.data)
+
+aa_list <- SplitObject(aa, split.by = "condition")
+
+aa_list <- lapply(X = aa_list, FUN = function(x) {
   x <- subset(x, subset = nFeature_RNA > 500 & percent.mt < 60)
   x <- NormalizeData(x)
   x <- FindVariableFeatures(x, selection.method = "vst", nfeatures = 2000)
 })
 
-features <- SelectIntegrationFeatures(object.list = all_list)
+features <- SelectIntegrationFeatures(object.list = aa_list)
 
-all_anchors <- FindIntegrationAnchors(object.list = all_list, anchor.features = features)
+aa_anchors <- FindIntegrationAnchors(object.list = aa_list, anchor.features = features)
 
-combined <- IntegrateData(anchorset = all_anchors)
-combined <- ScaleData(combined, features = rownames(all)) %>% RunPCA(npcs = 30, features = VariableFeatures(combined)) %>% RunUMAP(reduction = "pca", dims = 1:20, umap.method = 'umap-learn') %>% FindNeighbors(reduction = "pca", dims = 1:20) 
+combined <- IntegrateData(anchorset = aa_anchors)
+
+combined <- ScaleData(combined, features = rownames(aa)) %>% 
+  RunPCA(npcs = 30, features = VariableFeatures(combined)) %>% 
+  RunUMAP(reduction = "pca", dims = 1:20) %>%
+  FindNeighbors(reduction = "pca", dims = 1:20) 
 
 combined <- FindClusters(combined, resolution = 0.25)
 
-DimPlot(combined, reduction = "umap", pt.size = 1.5)
+DimPlot(combined, reduction = "umap", pt.size = 1.)
 
+
+saveRDS(combined, file = paste0(RdataDir, 'seuratObject_CCAIntegrated',  version.analysis, '.rds'))
+
+
+##########################################
+# subset the CMs 
+##########################################
+combined = readRDS
 DefaultAssay(combined) <- "RNA"
 
+
+
+
 CM <- subset(combined, idents = c("0"))
-CM <- FindVariableFeatures(CM, selection.method = "vst", nfeatures = 500) %>% ScaleData(features = rownames(CM))
+CM <- FindVariableFeatures(CM, selection.method = "vst", nfeatures = 500) %>% 
+  ScaleData(features = rownames(CM))
 CM <- RunPCA(CM, npcs = 30, features = VariableFeatures(CM)) %>% RunUMAP(reduction = "pca", dims = 1:5, umap.method = 'umap-learn') %>% FindNeighbors(reduction = "pca", dims = 1:5)
 CM <- FindClusters(CM, resolution = 0.2)
 
