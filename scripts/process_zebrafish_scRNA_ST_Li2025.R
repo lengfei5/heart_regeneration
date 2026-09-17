9ec9c967a41c94a512c2d8cad8d58f216129171a
jingkui.wang <jingkui.wang@imp.ac.at>
2026-09-17 14:29:12 +0200

    save the further analysis of zebrafish

--- scripts/process_zebrafish_scRNA_ST_Li2025.R
+++ scripts/process_zebrafish_scRNA_ST_Li2025.R
@@ -88,18 +88,18 @@ ggsave(filename = paste0('/groups/tanaka/Collaborations/Jingkui-Elad/Plots4manus
 
 aa$condition = aa$time_points
 
-p1 = DimPlot(aa, reduction = 'umap', raster = FALSE, group.by = 'celltypes', label = TRUE, repel = TRUE) 
+p1 = DimPlot(aa, reduction = 'umap', raster = FALSE, group.by = 'condition', label = TRUE, repel = TRUE) 
 p2 = DimPlot(aa, label = TRUE, repel = TRUE, group.by = 'annotation', raster=FALSE)
 
 p1 + p2
 
-ggsave(filename = paste0(resDir, 'zebrafish_Li2025_umap_celltypes_annotation.pdf'), 
+ggsave(filename = paste0(resDir, 'zebrafish_Li2025_umap_condition_annotation.pdf'), 
        width = 20, height = 8)
 
 saveRDS(aa, file = paste0(RdataDir, 'zebrafish_Li2025_scRNAseq_regeneration_all.rds'))
 
 
-## select only the CMs and plot AXL
+## select only the CMs and plot AXLs
 aa = subset(aa, cells = colnames(aa)[which(aa$celltypes == 'CMs')])
 
 aa <- NormalizeData(aa, normalization.method = "LogNormalize", scale.factor = 10000)
@@ -124,15 +124,25 @@ ggsave(filename = paste0('/groups/tanaka/Collaborations/Jingkui-Elad/Plots4manus
                          'zebrafish_Li2025_CMs_Axl.pdf'), width = 12, height = 10)
 
 
-
 ##########################################
 # an overview of zebrafish scRNA-seq data 
 ##########################################
-aa = readRDS(file = paste0(RdataDir, 'zebrafish_Li2025_scRNAseq_regeneration.rds'))
+aa = readRDS(file = paste0(RdataDir, 'zebrafish_Li2025_scRNAseq_regeneration_all.rds'))
 
 p1 = DimPlot(aa, label = TRUE, repel = TRUE, group.by = 'condition', raster=FALSE)
 p2 = DimPlot(aa, label = TRUE, repel = TRUE, group.by = 'annotation', raster=FALSE)
 
 p1 + p2
+
+## discard the blood cells
+aa = subset(aa, cells = colnames(aa)[which(aa$annotation != "Red blood cells")])
+
+aa <- NormalizeData(aa, normalization.method = "LogNormalize", scale.factor = 10000)
+aa <- FindVariableFeatures(aa, selection.method = "vst", nfeatures = 5000)
+
+aa <- RunPCA(aa, verbose = FALSE, weight.by.var = TRUE)
+ElbowPlot(aa, ndims = 30)
+
+aa <- RunUMAP(aa, dims = 1:20, n.neighbors = 50, min.dist = 0.3)
 
 
