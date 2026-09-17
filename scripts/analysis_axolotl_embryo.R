bad83e50037c3e333d91e640ccdd0a038789bbe8
jingkui.wang <jingkui.wang@imp.ac.at>
2026-09-17 13:16:49 +0200

    save the axolotl embryo first analysis

--- scripts/analysis_axolotl_embryo.R
+++ scripts/analysis_axolotl_embryo.R
@@ -57,7 +57,7 @@ seurat_obj <- LoadH5Seurat("data.h5seurat")
 
 
 ##########################################
-# try sceasy option
+# try sceasy option, didn't work either
 ##########################################
 library(sceasy)
 library(reticulate)
@@ -70,3 +70,78 @@ sceasy::convertFormat(paste0(dataDir, "/adata_cardiac_lineage_subset.h5ad"),
 
 
 
+##########################################
+# import the count table, metadata and also dimension reduction 
+##########################################
+counts = read.csv(paste0(RdataDir, '/cardiac_lineage_subset_adata_layer_counts.csv'), 
+                  header = TRUE, sep = ',', row.names = c(1))
+metadata = read.csv(paste0(RdataDir, '/cardiac_lineage_subset_adata_metadata.csv'), 
+                    header = TRUE, sep = ',', row.names = c(1))
+pca = read.csv(paste0(RdataDir, 'cardiac_lineage_subset_adata_rd_pca.csv'), 
+               header = TRUE, sep = ',', row.names = c(1))
+umap = read.csv(paste0(RdataDir, 'cardiac_lineage_subset_adata_rd_umap.csv'), 
+                header = TRUE, sep = ',', row.names = c(1))
+
+aa <- CreateSeuratObject(counts = t(as.matrix(counts)), project = "cardiacEmbryo", assay = "RNA",
+                         min.cells = 3, min.features = 50, meta.data = metadata)
+
+colnames(umap) <- paste0("UMAP_", 1:ncol(umap))
+aa[['umap_orig']] <- CreateDimReducObject(embeddings = as.matrix(umap), key = "UMAPorig_", 
+                                          assay = DefaultAssay(aa))
+
+colnames(pca) = paste0("PCA_", 1:ncol(pca))
+aa[['pca_orig']] <- CreateDimReducObject(embeddings = as.matrix(pca), key = "PCAorig_", 
+                                         assay = DefaultAssay(aa))
+#rm(list(c(counts, metadata, pca, umap)))
+rm(counts); rm(pca); rm(umap)
+
+saveRDS(aa, file = paste0(RdataDir, 'axolotlEmbryo_cardiac_lineage_subset.rds'))
+
+
+##########################################
+# processing the counts
+##########################################
+aa <- NormalizeData(aa, normalization.method = "LogNormalize", scale.factor = 10000)
+
+aa <- FindVariableFeatures(aa, selection.method = "vst", nfeatures = 5000)
+all.genes <- rownames(aa)
+aa <- ScaleData(aa, features = all.genes)
+aa <- RunPCA(aa, features = VariableFeatures(object = aa), verbose = FALSE)
+
+ElbowPlot(aa, ndims = 30)
+aa <- RunUMAP(aa, dims = 1:20, n.neighbors = 30, min.dist = 0.1)
+
+p1 = DimPlot(aa, label = TRUE, reduction = 'umap_orig', repel = TRUE, group.by = 'stage')
+
+p2 = DimPlot(aa, label = TRUE, reduction = 'umap_orig', repel = TRUE, group.by = 'cell_type')
+
+p1
+
+ggsave(filename = paste0(resDir, '/axolotlEmbryo_cardiakLineage_umap_stage.pdf'), 
+       width = 12, height = 8)
+
+p2
+
+ggsave(filename = paste0(resDir, '/axolotlEmbryo_cardiakLineage_umap_celltypes.pdf'), 
+       width = 12, height = 8)
+
+
+mtgenes = c("COX1", "COX2", "COX3", "ATP6", "ND1", "ND5", "CYTB", "ND2", "ND4", "ATP8", "MT-CO1", "COI")
+mtgenes = c(mtgenes, paste0("MT", mtgenes), paste0("MT-", mtgenes))
+mtgenes = rownames(aa)[!is.na(match(rownames(aa), mtgenes))]
+
+xx = PercentageFeatureSet(aa, col.name = "percent.mt", assay = "RNA", features = mtgenes)
+aa[['percent.mt']] = xx$percent.mt
+
+rm(xx)
+
+
+FeaturePlot(aa, features = c("nCount_RNA", 'nFeature_RNA', 'percent.mt'))
+
+ggsave(filename = paste0(resDir, '/axolotlEmbryo_cardiakLineage_umap_QCs.pdf'), 
+       width = 12, height = 16)
+
+VlnPlot(aa ,features = c("nCount_RNA", 'nFeature_RNA', 'percent.mt'), group.by = 'stage')
+
+ggsave(filename = paste0(resDir, '/axolotlEmbryo_cardiakLineage_vlnplot_QCs.pdf'), 
+       width = 20, height = 8)
